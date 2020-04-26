0773c2b73f2ceb0ba2e5fc6856887f0f0f69aa8e
Eric Pahl <pahle93@gmail.com>
2020-04-26 23:22:14 +0000

    non-ascii chars deleted in comments

--- cart.py
+++ cart.py
@@ -25,8 +25,8 @@ class DecisionTreeClassifier:
     def _gini(self, y):
         """Compute Gini impurity of a non-empty node.
 
-        Gini impurity is defined as Σ p(1-p) over all classes, with p the frequency of a
-        class within the node. Since Σ p = 1, this is equivalent to 1 - Σ p^2.
+        Gini impurity is defined as sigma p(1-p) over all classes, with p the frequency of a
+        class within the node. Since sigma p = 1, this is equivalent to 1 - sigma p^2.
         """
         m = y.size
         return 1.0 - sum((np.sum(y == c) / m) ** 2 for c in range(self.n_classes_))
